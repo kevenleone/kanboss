ad63e8f48d964c3953778b75bb511c328f743ffa
Keven Leone <keven.leone@liferay.com>
2022-10-19 17:28:07 -0300

    Add FontFamily according to Theme #4

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,9 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["github.com"],
+  },
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ["en"],
